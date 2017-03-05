090eb9fe9ab709a5fb3b55b72fa977b6b44594e8
codeyu <codeyu@gmail.com>
2017-03-05 15:52:06 +0800

    init

---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = JumpHash.JumpConsistentHash(3, 15);
+            Console.WriteLine($"buckets: {result}");
+            result = JumpHash.JumpConsistentHash(3, 17);
+            Console.WriteLine($"buckets: {result}");
+
+            var ip = "192.168.1.10";
+            Console.WriteLine(ip.GetHashCode());
+            result = JumpHash.JumpConsistentHash(ip, 20);
+            Console.WriteLine($"buckets: {result}");
+            Console.WriteLine(ip.GetHashCode());
+            result = JumpHash.JumpConsistentHash(ip, 22);
+            Console.WriteLine($"buckets: {result}");
+        }
+    }
+}
